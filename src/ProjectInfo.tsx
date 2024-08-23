029915d510e57355b836d6af845d6f21f55dde34
BaixiGuo <steven20001108@gmail.com>
2024-08-23 13:59:23 -0700

    Finish First Component

---
+++ src/ProjectInfo.tsx
@@ -0,0 +1,21 @@
+// 1. Weather API
+// 2. Material UI, Tailwind CSS
+// 3. Github Submission
+
+import React, {useState} from 'react';
+
+const ProjectInfo: React.FC = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+       <div>
+            <h1> Build Project: React Application</h1>
+            <h2> Baixi Guo</h2>
+            <p> Counts: {count >= 0? count: 0}</p>
+            <button onClick = {() => setCount(count + 1)}> Increment + </button>
+            <button onClick = {() => setCount(count - 1)}> Decrement - </button>
+       </div>
+    );
+};
+
+export default ProjectInfo;
